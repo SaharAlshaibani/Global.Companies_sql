90e368ec70cbc503a27de69e5f7d0643cabeaab8
Sahar <sahar.marq16.6@gmail.com>
2025-09-16 23:06:08 +0300

    Create sample_insert.sql

---
+++ sql/sql/data/sample_insert.sql
@@ -0,0 +1,6 @@
+INSERT INTO mytable (A,B,C,D,E,F,G) VALUES
+(1,'Microsoft','MSFT','$3.033 T','$407.21','🇺🇸','USA'),
+(2,'Apple','AAPL','$2.951 T','$190.92','🇺🇸','USA'),
+(3,'Saudi Aramco','2222.SR','$2.026 T','$8.34','🇸🇦','Saudi Arabia'),
+(4,'NVIDIA','NVDA','$2.010 T','$824.86','🇺🇸','USA'),
+(5,'Alphabet (Google)','GOOGL','$1.940 T','$155.76','🇺🇸','USA');
